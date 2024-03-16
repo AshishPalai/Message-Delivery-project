01a08de321f6e26786c8ba4728def58b8e0d28c7
Ashish Kumar Palai <162289652+AshishPalai@users.noreply.github.com>
2024-03-16 23:04:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+(function(){
+    const form = document.querySelector("#messageForm")
+    form.addEventListener( 'submit', function(e){
+        e.preventDefault()
+        const message= document.querySelector('#message')
+        const feedback= document.querySelector('.feedback')
+        const messagecontent= document.querySelector('.messagecontent')
+
+        if(message.value === ''){
+            feedback.classList.add('show')
+            setTimeout(function(){
+                feedback.classList.remove('show')
+            }, 4000)
+        }else {
+            messagecontent.textContent = message.value
+            message.value =''
+        }
+    })
+})()
